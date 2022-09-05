6a404f014625307fa8877ef9a91169be6f579936
desmond58 <chiping5858@gmail.com>
2022-09-05 09:23:54 +0800

    js for lab 3.1

---
+++ Lab3/Task_3.1.js
@@ -0,0 +1,60 @@
+function init(){
+	var w = 500;
+	var h = 100;
+	//var barPadding = 2;
+	var dataset =  [[5, 20], 
+					[480, 90], 
+					[250, 50], 
+					[100, 33], 
+					[330, 95], 
+					[410, 12], 
+					[475, 44], 
+					[25, 67], 
+					[85, 21], 
+					[220, 88] 
+					];
+	
+    var scale = d3.scaleLinear()
+                    .domain([100,500])
+                    .range([10,350]);
+
+	var svg = d3.select("body")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h);
+	
+	svg.selectAll("circle")
+		.data(dataset)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d,i){
+			return d[0];
+		})
+		.attr("cy", function (d){ 
+			return d[1];
+		})
+		.attr("r", 5)
+		.attr("fill", function(d){
+			if(d[0] == 480){
+				return "red";
+			}else{
+				return "slategrey";
+			}
+		});
+
+	svg.selectAll("text")
+		.data(dataset)
+		.enter()
+		.append("text")
+		.text(function(d){
+			return d[0] + "," + d[1];
+		})
+		.attr("x", function (d) {
+			return d[0];
+		})
+		.attr("y", function (d) {
+			return d[1];
+		});
+}
+
+window.onload = init;
